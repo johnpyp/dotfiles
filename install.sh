6c8bfaf54f5882973817fae20fc74d1f82ec4ba6
johnpyp <johnpaulpenaloza@gmail.com>
2018-12-22 11:11:08 -0500

    Moved dotfiles into dedicated 'dots' folder

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-dotfiles=(".zshrc" ".gitconfig", ".zpreztorc")
+dotfiles=(".zshrc" ".gitconfig" ".zpreztorc")
 
 mkdir -p backups
 for dotfile in "${dotfiles[@]}"
 do
-  if [ -f ./${dotfile} ]; then
-    if [ ! "`readlink ${HOME}/${dotfile}`" -ef "${PWD}/${dotfile}" ]; then
+  if [ -f ${PWD}/dots/${dotfile} ]; then
+    if [ ! "`readlink ${HOME}/${dotfile}`" -ef "${PWD}/dots/${dotfile}" ]; then
       if [ -f ${HOME}/${dotfile} ]; then
         mv -f ${HOME}/${dotfile} ${PWD}/backups
       fi
-      ln -sv "${PWD}/${dotfile}" ${HOME}
+      ln -sv "${PWD}/dots/${dotfile}" ${HOME}
     fi
   fi
 
