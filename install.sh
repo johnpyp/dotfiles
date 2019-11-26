c3f443a4abf2f9fa584ac8b9776c6009b8c6fb60
johnpyp <johnpaulpenaloza@gmail.com>
2019-11-26 08:32:27 -0500

    Added ovpn thing

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-dotfiles=(".zshrc" ".gitconfig" ".Xresources" ".vimrc" ".ripgreprc" ".ideavimrc")
+dotfiles=(".zshrc" ".gitconfig" ".Xresources" ".vimrc" ".ripgreprc" ".ideavimrc" ".vpn")
 config_dotdirectories=("i3" "polybar" "pscripts" "networkmanager-dmenu" "wallpapers" "nvim" "kitty" "redshift.conf")
 
 mkdir -p backups
 for dotfile in "${dotfiles[@]}"
 do
-  if [ -f ${PWD}/dots/${dotfile} ]; then
+  if [ -e ${PWD}/dots/${dotfile} ]; then
     if [ ! "`readlink ${HOME}/${dotfile}`" -ef "${PWD}/dots/${dotfile}" ]; then
-      if [ -f ${HOME}/${dotfile} ]; then
-        mv -f ${HOME}/${dotfile} ${PWD}/backups
+      if [ -e ${HOME}/${dotfile} ]; then
+        mv ${HOME}/${dotfile} ${PWD}/backups
       fi
       ln -sv "${PWD}/dots/${dotfile}" ${HOME}
     fi
