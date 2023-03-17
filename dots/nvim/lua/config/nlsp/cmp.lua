aefdb30d44728f4f80b689588356c412d1a6903e
johnpyp <johnpyp.dev@gmail.com>
2023-03-16 20:37:30 -0700

    Better cmp config + rust

--- dots/nvim/lua/config/nlsp/cmp.lua
+++ dots/nvim/lua/config/nlsp/cmp.lua
@@ -1,24 +1,81 @@
 local lspkind = require("lspkind")
 local lsp = require("lsp-zero")
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 local M = {}
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ["<C-Space>"] = cmp.mapping.complete(),
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    elseif has_words_before() then
+      cmp.complete()
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
+  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
 })
 
-function M.create_cmp_config()
-  return {
-    sources = {
+local kind_mapper = require("cmp.types").lsp.CompletionItemKind
+
+local kind_score = {
+  Variable = 1,
+  Class = 2,
+  Property = 3,
+  Field = 3,
+  Method = 4,
+  Keyword = 5,
+}
+
+function M.setup_cmp()
+  local cmp_config = lsp.defaults.cmp_config({
+    sources = cmp.config.sources({
+      {
+        name = "nvim_lsp",
+        entry_filter = function(entry, context)
+          local kind = entry:get_kind()
+          local line = context.cursor_line
+          local col = context.cursor.col
+
+          local char_before_cursor = string.sub(line, col - 1, col - 1)
+
+          if char_before_cursor == "." then
+            if kind == kind_mapper["Method"] or kind == kind_mapper["Field"] or kind == kind_mapper["Property"] then
+              return true
+            else
+              return false
+            end
+          end
+
+          return true
+        end,
+      },
+      { name = "luasnip", keyword_length = 1, max_item_count = 5 },
       { name = "path" },
-      { name = "nvim_lsp" },
-      { name = "buffer", keyword_length = 3 },
-      { name = "luasnip" },
-    },
+    }, {
+      { name = "buffer", keyword_length = 3, max_item_count = 5 },
+    }),
     mapping = cmp_mappings,
     formatting = {
-      fields = { "abbr", "kind" },
+      fields = { "abbr", "kind", "menu" },
       -- format = require("config.lsp.kind").cmp_format(),
       format = function(entry, vim_item)
         if vim.tbl_contains({ "path" }, entry.source.name) then
@@ -29,10 +86,87 @@ function M.create_cmp_config()
             return vim_item
           end
         end
-        return lspkind.cmp_format({ with_text = false })(entry, vim_item)
+        local menu_text = {
+          nvim_lsp = "LSP",
+          luasnip = "SNIPPET",
+          buffer = "BUFFER",
+          path = "PATH",
+          nvim_lua = "LUA",
+        }
+
+        return lspkind.cmp_format({
+          -- symbol_map = require("config.lsp.kind").icons,
+          preset = "default",
+          mode = "symbol_text",
+          menu = menu_text,
+        })(entry, vim_item)
       end,
     },
-  }
+
+    sorting = {
+      priority_weight = 2,
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+
+        function(entry1, entry2)
+          local kind1 = kind_mapper[entry1:get_kind()]
+          local kind2 = kind_mapper[entry2:get_kind()]
+
+          local kind_score1 = kind_score[kind1] or 100
+          local kind_score2 = kind_score[kind2] or 100
+
+          if kind_score1 < kind_score2 then return true end
+          if kind_score1 > kind_score2 then return false end
+        end,
+        -- copied from cmp-under, but I don't think I need the plugin for this.
+        -- Properly sorts underscore properties
+        -- function(entry1, entry2)
+        --   local _, entry1_under = entry1.completion_item.label:find("^_+")
+        --   local _, entry2_under = entry2.completion_item.label:find("^_+")
+        --   entry1_under = entry1_under or 0
+        --   entry2_under = entry2_under or 0
+        --   if entry1_under > entry2_under then
+        --     return false
+        --   elseif entry1_under < entry2_under then
+        --     return true
+        --   end
+        -- end,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    },
+  })
+
+  cmp.setup(cmp_config)
+end
+
+function M.setup_cmp_highlights()
+  vim.cmd([[
+  " gray
+  highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+  " blue
+  highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+  highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+  " light blue
+  highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+  highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+  highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+  " pink
+  highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+  highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+  " front
+  highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+  highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+  highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
+
+  highlight! link CmpItemMenu CmpItemMenuDefault
+]])
 end
 
 return M
--- dots/nvim/lua/config/nlsp/init.lua
+++ dots/nvim/lua/config/nlsp/init.lua
@@ -2,7 +2,7 @@ require("config.nlsp.mason").setup_mason_ui()
 
 local lsp = require("lsp-zero")
 
-lsp.preset("recommended")
+lsp.preset("lsp-compe")
 
 require("lspconfig.ui.windows").default_options.border = "rounded"
 vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "FloatBorder" })
@@ -19,7 +19,8 @@ lsp.set_server_config({
 
 lsp.on_attach(require("config.nlsp.on_attach").create_on_attach())
 
-lsp.setup_nvim_cmp(require("config.nlsp.cmp").create_cmp_config())
+require("config.nlsp.cmp").setup_cmp()
+require("config.nlsp.cmp").setup_cmp_highlights()
 
 local custom_settings = require("config.nlsp.settings")
 
@@ -29,12 +30,52 @@ lsp.configure("tsserver", custom_settings.tsserver)
 
 require("neodev").setup()
 
-local rust_lsp = lsp.build_options("rust_analyzer", {})
+local rust_lsp_opts = lsp.build_options("rust_analyzer", custom_settings.rust_analyzer)
+local null_opts = lsp.build_options("null-ls", {})
 
 lsp.setup()
 
 -- Initialize rust_analyzer with rust-tools
-require("rust-tools").setup({ server = rust_lsp })
+require("rust-tools").setup({
+
+  -- inlay_hints = {
+
+  --   --     -- Only show inlay hints for the current line
+  --   --     only_current_line = false,
+
+  --   --     -- Event which triggers a refersh of the inlay hints.
+  --   --     -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+  --   --     -- not that this may cause  higher CPU usage.
+  --   --     -- This option is only respected when only_current_line and
+  --   --     -- autoSetHints both are true.
+  --   --     only_current_line_autocmd = "CursorHold",
+
+  --   --     -- wheter to show parameter hints with the inlay hints or not
+  --   --     show_parameter_hints = true,
+
+  --   --     -- prefix for parameter hints
+  --   parameter_hints_prefix = "  <-  ",
+
+  --   --     -- prefix for all the other hints (type, chaining)
+  --   other_hints_prefix = "  =>  ",
+
+  --   --     -- whether to align to the length of the longest line in the file
+  --   -- max_len_align = true,
+
+  --   --     -- padding from the left if max_len_align is true
+  --   -- max_len_align_padding = 10,
+
+  --   --     -- whether to align to the extreme right or not
+  --   --     right_align = false,
+
+  --   --     -- padding from the right if right_align is true
+  --   --     right_align_padding = 7,
+
+  --   --     -- The color of the hints
+  --   highlight = "LspCodeLens",
+  -- },
+  server = rust_lsp_opts,
+})
 
 -- Setup crates.nvim for cargo.toml files
 require("crates").setup()
@@ -45,9 +86,8 @@ require("config.nlsp.diagnostics").setup_config_diagnostics()
 -- Make sure nls is imported *after* lsp setup
 -- https://github.com/VonHeikemen/lsp-zero.nvim/issues/60#issuecomment-1363800412
 
-local null_opts = lsp.build_options("null-ls", {})
-
 require("config.nlsp.null_ls").setup_null_ls(null_opts)
-require("config.nlsp.diagnostics").setup_diagnostic_higlights()
 
 require("config.nlsp.autopairs").setup_autopairs()
+
+require("fidget").setup()
