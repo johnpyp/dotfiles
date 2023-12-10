28a760149b919ec1280a40de3f1504a4c99d47c5
johnpyp <johnpyp.dev@gmail.com>
2023-12-10 14:48:43 -0800

    nixos: accept nvidia license

--- nix/modules/packages/base.nix
+++ nix/modules/packages/base.nix
@@ -1,9 +1,6 @@
-{ config, pkgs, ... }:
-{
-  imports = [
-    ../../overrides
-    ../../overlays
-  ];
+{ config, pkgs, ... }: {
+  imports = [ ../../overrides ../../overlays ];
 
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
 }
