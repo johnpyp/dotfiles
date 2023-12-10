ace4d735b0f9461dff60249988294b0da3fd7916
johnpyp <johnpyp.dev@gmail.com>
2023-12-10 14:48:52 -0800

    nixos: use local nameservers

--- nix/modules/networking.nix
+++ nix/modules/networking.nix
@@ -1,8 +1,7 @@
-{ config, pkgs, lib, ... }:
-{
+{ config, pkgs, lib, ... }: {
   networking = {
     useDHCP = lib.mkDefault true;
     networkmanager.enable = true;
-    nameservers = [ "192.168.1.1" "8.8.8.8" "8.8.4.4" "1.1.1.1" "1.0.0.1" ];
+    nameservers = [ "192.168.1.1" "192.168.1.2" ];
   };
 }
