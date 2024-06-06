c9dbccff696ec2b0319a5db5d434b40ab31bc851
johnpyp <johnpyp.dev@gmail.com>
2024-06-05 22:37:56 -0700

    prioritize underscore methods lower than non-underscore

--- dots/nvim/lua/config/nlsp/cmp.lua
+++ dots/nvim/lua/config/nlsp/cmp.lua
@@ -255,6 +255,17 @@ function M.setup_cmp()
         cmp.config.compare.locality,
 
         function(entry1, entry2)
+          local _, entry1_under = entry1.completion_item.label:find("^_+")
+          local _, entry2_under = entry2.completion_item.label:find("^_+")
+          entry1_under = entry1_under or 0
+          entry2_under = entry2_under or 0
+          if entry1_under > entry2_under then
+            return false
+          elseif entry1_under < entry2_under then
+            return true
+          end
+        end,
+        function(entry1, entry2)
           local kind1 = kind_mapper[entry1:get_kind()]
           local kind2 = kind_mapper[entry2:get_kind()]
 
