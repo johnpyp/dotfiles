387eecb4829601da78ed966ae3a537592517ef64
johnpyp <johnpyp.dev@gmail.com>
2024-06-26 15:59:34 -0700

    add bash formatter

--- dots/nvim/lua/oko/langs/bash/init.lua
+++ dots/nvim/lua/oko/langs/bash/init.lua
@@ -8,5 +8,15 @@ return {
       optional = true,
       opts = { ensure_installed = { "shellcheck", "shfmt" } }
     },
+    {
+      "stevearc/conform.nvim",
+      optional = true,
+      opts = {
+        formatters_by_ft = {
+          bash = { "shfmt" },
+          sh = { "shfmt" },
+        },
+      },
+    },
   }
 }
